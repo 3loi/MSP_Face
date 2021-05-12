7a969c3969f53938c1a92dbdb1408ea8544f796d
3loi <43795050+3loi@users.noreply.github.com>
2021-05-12 17:50:25 -0500

    Update config_file.py

--- Visual_Modality/config_file.py
+++ Visual_Modality/config_file.py
@@ -1,1 +1,22 @@
-alsdfj
+#FEAT_PATH dir to save files
+FEAT_PATH = './VGG_face_feat_8class'
+IMAGE_PATH = '/home/a/Desktop/MSP-Face/jpg_face/*/*'
+
+#paths for the VGG_MODEL
+MEANS_PATH = './affectnet_8class/output/affectnet_emo_mean.json'
+VGG_MODEL_PATH = './affectnet_8class/output/affectnet_8emo.model'
+
+IMAGE_WIDTH = 256
+IMAGE_HEIGHT = 256
+
+
+# user None for VAD 
+NUM_CLASSES = 8
+# NUM_CLASSES = 5
+# NUM_CLASSES = None
+
+#consensus path and class ID path (provided in the github)
+LABEL_PATH = './Doc/label_consensus.json'
+NAME_PATH = './Doc/classInd_8class.txt'
+
+MODEL_PATH = 'output/8_CLASS_LSTM'
